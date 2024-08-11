e43824953af97217cf65210ae67e0bd92908b295
Alex Campos <alexcampos@Alexs-Air.attlocal.net>
2024-08-11 00:41:48 -0500

    binding

--- practice/src/app/test/test.component.ts
+++ practice/src/app/test/test.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
@@ -13,6 +16,16 @@ export class TestComponent {
   public myId: string  = "testId"
   // Binding
   public isDisabled: boolean = false;
+  public successClass: string = "text-success";
+  public hasError: boolean = false;
+
+  public isSpecial: boolean = false;
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial,
+
+  }
   greetUser() {
     return "Hello " + this.name;
   }
